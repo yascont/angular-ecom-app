e25983ab400421547788ddc94b53dfbc93219588
Yassine <control.yassine@gmail.com>
2024-04-03 22:33:37 +0000

    feat: add cartPage

--- src/pages/cart-page/cart-page.component.ts
+++ src/pages/cart-page/cart-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CartProductComponent } from '../../components/cart-product/cart-product.component';
 
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [],
+  imports: [CartProductComponent],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css'
 })
