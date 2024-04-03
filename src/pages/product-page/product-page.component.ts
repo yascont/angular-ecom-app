9f68d9f61bf665ac576ef57149bba86191bad231
Yassine <control.yassine@gmail.com>
2024-04-03 22:35:00 +0000

    feat: add ProductPage

--- src/pages/product-page/product-page.component.ts
+++ src/pages/product-page/product-page.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { CategoryCardsComponent } from '../../components/category-cards/category-cards.component';
+import { fetchProducts } from '../../app/fetshProductsService';
+import { ActivatedRoute } from '@angular/router';
+import { inject } from '@angular/core';
+
+
+
+interface Product {
+  id?: number;
+  title?: string;
+  price?: number;
+  description?: string;
+  category?: string;
+  image?: string;
+  rating?: {
+    rate?: number;
+    count?: number;
+  };
+}
+
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [],
+  imports: [CategoryCardsComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css'
 })
 export class ProductPageComponent {
+  private activatedRoute = inject(ActivatedRoute);
+  private fetch = inject(fetchProducts)
+  
+  product : Product = {};
+  id: string = '';
 
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.id = params['id'];
+      this.fetchProducts();
+    });
+  }
+
+  fetchProducts() {
+      this.fetch.getSpeceficProducts(parseInt(this.id)).subscribe((prod: Product) =>
+         this.product = prod);
+  }
 }
+
