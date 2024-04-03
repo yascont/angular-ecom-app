2fb9ba58a4b472a34b9696996e165ba7a3d90178
Yassine <control.yassine@gmail.com>
2024-04-03 22:32:17 +0000

    feat: add HomePgae

--- src/pages/home-page/home-page.component.ts
+++ src/pages/home-page/home-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { HomeHeadComponent } from '../../components/home-head/home-head.component';
+import { CategoryCardsComponent } from '../../components/category-cards/category-cards.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [HomeHeadComponent, CategoryCardsComponent, RouterLink],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
+  providers: []
 })
 export class HomePageComponent {
-
 }
